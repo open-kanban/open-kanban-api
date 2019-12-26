c20e4c197dac78bac464bf1614be5a0ac90ab5ac
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2019-12-26 17:09:54 +0100

    refactor: remove user model

--- src/app/use-cases/users/index.ts
+++
@@ -1,8 +0,0 @@
-import { UserData } from '../../entities/user';
-
-export type UserModel = {
-  findByEmail: (email: string) => Promise<Required<UserData> | null>;
-  findById: (userId: string) => Promise<Required<UserData> | null>;
-  save: (userData: Required<UserData>) => Promise<Required<UserData>>;
-  isAuthorizedToModifyColumn: (userId: string, columnId: string) => Promise<boolean>;
-};
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,7 @@
 import makeBoardModel from './board/board-model';
 import makeCardModel from './card/card-model';
 import makeColumnModel from './column/column-model';
-import makeUserModel from './user/user-model';
 
-export const userModel = makeUserModel();
 export const boardModel = makeBoardModel();
 export const columnModel = makeColumnModel();
 export const cardModel = makeCardModel();
